ed4abf5f55ea953973a02c996f84507b5fe08fab
Dianna Tingg <dtingg@github.com>
2021-05-04 10:59:12 -0700

    Finished Extra challenges for Exercise A

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
-    const STARTING_MISSILES:i32 = 8;
-    const READY_AMOUNT:i32 = 2;
+    const STARTING_MISSILES: i32 = 8;
+    const READY_AMOUNT: i32 = 2;
 
-    let mut missiles = STARTING_MISSILES;
-    let ready = READY_AMOUNT;
+    let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {} of my {} missiles...", ready, missiles);
 
     missiles = missiles - ready;
     println!("{} missiles left", missiles);
+
+    // This warns that missiles does not need to be mutable.
+    // println!("{} missiles left", missiles - ready);
+
+    // This warns that extra is an unused variable.
+    // let extra = 5;
+
+    // This warns invalid left-hand side of assignment
+    // READY_AMOUNT = 3;
 }
