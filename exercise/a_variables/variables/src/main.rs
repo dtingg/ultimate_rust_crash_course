b3a45b136bf2eb84f955b21659206f84a05ff55b
Dianna Tingg <dtingg@github.com>
2021-05-04 14:40:17 -0700

    Move constants out of main

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,6 +1,9 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-    const STARTING_MISSILES: i32 = 8;
-    const READY_AMOUNT: i32 = 2;
+    // let missiles = 8;
+    // let ready = 2;
 
     let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {} of my {} missiles...", ready, missiles);
